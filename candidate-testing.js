fa99f4a3f2722dafb435ea21a3ac6593ae6dad2f
AwolE <>
2021-06-04 05:01:04 +0000

    ask for name

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,37 +1,41 @@
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
+// TODO 1.1a: Define candidateName //
 
-// TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName= ""
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question= "Who was the first American woman in space?";
+let correctAnswer="Sally Ride";
+let candidateAnswer="";
+
 let questions;
 let correctAnswers;
 let candidateAnswers;
 
-
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+  candidateName= input.question("Enter your Name?");
+  console.log("Hello,",candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+  candidateAnswer= input.question(question);
+  //console.log("your answer is: " + candidateAnswer);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
+ if (candidateAnswer===correctAnswer) {
+    console.log("Your answer is Correct!");
+  } else {
+    console.log("Your answer is Wrong.");
+  }
 
   let grade;
-  
-
   return grade;
 }
 
@@ -42,6 +46,11 @@ function runProgram() {
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
+
+//Part 2: Multiple Questions
+ 
+
+
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
