80fe0edeb5a832a97a050d3a1035cd5b98e94273
AwolE <85002171+Awol-E@users.noreply.github.com>
2021-06-11 01:18:13 +0000

    Worked on part 2

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,38 +3,56 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 // TODO 1.1a: Define candidateName //
 
-let candidateName= ""
+let candidateName= "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question= "Who was the first American woman in space?";
+let question= "Who was the first American woman in space? ";
 let correctAnswer="Sally Ride";
 let candidateAnswer="";
 
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer === 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ", 
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", 
+  'What is the minimum crew size for the ISS? '
+  ];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers =[];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName= input.question("Enter your Name?");
-  console.log("Hello,",candidateName);
+  candidateName = input.question("Enter your Name? ");
+  console.log("Hello, ",candidateName);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  candidateAnswer= input.question(question);
-  //console.log("your answer is: " + candidateAnswer);
+
+  for(let j=0; j < questions.length; j++) {
+    candidateAnswers[j] = input.question(questions[j] + '\n' + "Your Answer: ");
+    //candidateAnswers[j] = input.question(questions[j])
+    //console.log("Your Answer: "+ candidateAnswers[j])
+    console.log("Correct Answer: "+ correctAnswers[j])
+    console.log("\n");  
+  }
+  //console.log(candidateAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
- if (candidateAnswer===correctAnswer) {
-    console.log("Your answer is Correct!");
+ 
+ for(j=0; j < questions.length; j++) {
+  if (candidateAnswers[j].toLowerCase() === correctAnswers[j].toLocaleLowerCase()) 
+ {
+   let i = j + 1;
+    console.log("Your answer is Correct for question # " + i);
   } else {
-    console.log("Your answer is Wrong.");
+    i = j + 1;
+    console.log("Your answer is Wrong for question # " + i);
   }
-
+ }
   let grade;
   return grade;
 }
@@ -42,15 +60,11 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
 
 //Part 2: Multiple Questions
- 
-
-
 
 // Don't write any code below this line //
 // And don't change these or your program will not run as expected //
